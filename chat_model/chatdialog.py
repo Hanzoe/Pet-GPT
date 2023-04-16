9413f4224942970a16d6fbe8641ea049c00d131e
Hanzoe <2500066889@qq.com>
2023-04-16 17:37:43 +0800

    优化聊天界面 1. 聊天界面分为3个角色（我，宠物，系统） 2. 我和宠物增加头像 3. 按钮的命令都是系统命令

--- chat_model/chatdialog.py
+++ chat_model/chatdialog.py
@@ -1,8 +1,8 @@
-from PyQt5.QtWidgets import QDialog, QVBoxLayout, \
-    QTextEdit, QPushButton,  QHBoxLayout, QComboBox, QPlainTextEdit, QMainWindow,  QFrame, QDesktopWidget
-from PyQt5.QtCore import Qt, pyqtSignal, QThread, QEvent
+from PyQt5.QtWidgets import QDialog, QVBoxLayout, QSizePolicy,\
+    QTextEdit, QPushButton,  QHBoxLayout, QComboBox, QPlainTextEdit, QMainWindow,  QFrame, QDesktopWidget, QLabel,QWidget, QScrollArea, QGridLayout, QSpacerItem
+from PyQt5.QtCore import Qt, pyqtSignal, QThread, QEvent, QSize, QTimer
 from .openai_api import OpenAI_API
-from PyQt5.QtGui import QKeyEvent
+from PyQt5.QtGui import QKeyEvent, QPixmap, QFontMetrics
 import datetime
 import os
 import sys
@@ -116,7 +116,7 @@ class ChatWindow(QMainWindow):
 
     #通用函数
     def send_to_gpt(self,message):
-        self.chat_dialog_body.append_message(message=f'我:{message}',is_user=True)
+        self.chat_dialog_body.add_message("system",message)
         # 更新聊天上下文
         self.chat_dialog_body.context_history += f"user: {message}\n"
         prompt = message
@@ -148,6 +148,75 @@ class ChatWindow(QMainWindow):
     def function_4(self):
         print("我是4号")
 
+#聊天框的主体部分
+class ChatWidget(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        
+        self.scroll_area = QScrollArea(self)
+        self.scroll_area.setWidgetResizable(True)
+        self.scroll_area.setFocusPolicy(Qt.NoFocus)
+        self.scroll_area.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.scroll_area.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)  # 只显示垂直滚动条
+        
+        self.container = QWidget(self.scroll_area)
+        self.container_layout = QVBoxLayout(self.container)
+        self.container_layout.setContentsMargins(0, 0, 0, 0)
+        self.container_layout.setSpacing(0)  # 设置控件之间的间距为 0
+        
+        self.scroll_area.setWidget(self.container)
+
+        layout = QHBoxLayout(self)
+        layout.addWidget(self.scroll_area)
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.addSpacing(10)
+    
+
+
+class MessageWidget(QWidget):
+    def __init__(self, role, text, parent=None):
+        super().__init__(parent)
+        self.role = role
+        self.text = text
+        # 图像
+        self.label = QLabel(self)
+        self.label.setAlignment(Qt.AlignCenter)
+        avatar = QPixmap("image\\avatar_{}.png".format(role)).scaledToWidth(30).scaledToHeight(30)
+        self.label.setPixmap(avatar)
+        # 文字
+        self.text_label = QLabel(self)
+        self.text_label.setWordWrap(True)
+        self.text_label.setText(text)
+        self.text_label.setTextInteractionFlags(Qt.TextSelectableByMouse)
+
+        # 设置大小策略
+        # size_policy = QSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
+        # size_policy.setHeightForWidth(True)
+        # self.text_label.setSizePolicy(size_policy)
+        
+        # 使用 QGridLayout 布局来实现宽度比例的设置
+        layout = QGridLayout(self)
+        layout.addWidget(self.label, 0, 1, 2, 1, Qt.AlignTop)
+        layout.addWidget(self.text_label, 0, 2, 1, 3, Qt.AlignTop)
+        layout.addItem(QSpacerItem(20, 20, QSizePolicy.Fixed, QSizePolicy.Expanding), 0, 0, 1, 1)
+        layout.addItem(QSpacerItem(20, 20, QSizePolicy.Expanding, QSizePolicy.Fixed), 0, 5, 1, 1)
+        layout.setColumnStretch(2, 3)
+        layout.setColumnStretch(3, 5)
+        layout.setColumnMinimumWidth(4, 20)
+        layout.setColumnStretch(5, 1)
+        layout.setContentsMargins(0, 0, 0, 0)
+
+        # 设置最大高度，使其与最小高度一致
+        self.setMaximumHeight(self.sizeHint().height()) 
+
+    def sizeHint(self):
+        fm = QFontMetrics(self.text_label.font())
+        text_width = self.text_label.sizeHint().width()
+        text_height = fm.size(Qt.TextWordWrap, self.text_label.text()).height() + fm.descent() + 5
+        return QSize(text_width + self.label.sizeHint().width() + 40, text_height + 20)
+
+
+
 class ChatDialogBody(QDialog):
     def __init__(self, config, parent=None):
         super().__init__(parent)
@@ -178,8 +247,7 @@ class ChatDialogBody(QDialog):
         self.open_ai.response_received.connect(self.handle_response)
         layout = QVBoxLayout()
 
-        self.chat_history = QTextEdit()
-        self.chat_history.setReadOnly(True)
+        self.chat_history = ChatWidget()
         layout.addWidget(self.chat_history)
         
         chat_input_layout = QHBoxLayout()
@@ -241,28 +309,48 @@ class ChatDialogBody(QDialog):
                 return True
         return super().eventFilter(source, event)
 
-    #用来区别身份的聊天记录
-    def append_message(self, message, is_user=False):
-        """
-        向聊天历史记录中添加消息，并设置不同颜色
-        """
-        color = "red" if is_user else "black"
-        message = f"<font color='{color}'>{message}</font>"
-        self.chat_history.insertHtml(message + "<br>")
-        
+    # #用来区别身份的聊天记录
+    # def append_message(self, message, is_user=False):
+    #     """
+    #     向聊天历史记录中添加消息，并设置不同颜色
+    #     """
+    #     color = "red" if is_user else "black"
+    #     message = f"<font color='{color}'>{message}</font>"
+    #     self.chat_history.insertHtml(message + "<br>")
+    
+    #窗口增加信息的统一模块
+    def add_message(self,role,text):
+        #封装成组件
+        message=MessageWidget(role,text)
+        self.chat_history.container_layout.addWidget(message)
+         # 分割线
+        line = QFrame(self)
+        line.setFrameShape(QFrame.HLine)
+        line.setFrameShadow(QFrame.Sunken)
+        self.chat_history.container_layout.addWidget(line)  # 将分隔线添加到布局中
+        # 强制更新容器的大小
+        self.chat_history.container.adjustSize()
+        # 滚动条到最下面
+        def scroll():
+            max_value = self.chat_history.scroll_area.verticalScrollBar().maximum()
+            self.chat_history.scroll_area.verticalScrollBar().setValue(max_value)
+
+        QTimer.singleShot(0, scroll)
+        # 保存聊天记录到本地
+        self.save_chat_history(message)
+
     #按下发送按钮后的事件
     def send_message(self):
-        message = self.message_input.toPlainText()
-        if message:
-            self.append_message(message=f'我:{message}',is_user=True)
+        text = self.message_input.toPlainText()
+        role = "user"
+        if text:
+            self.add_message(role,text)
             # 更新聊天上下文
-            self.context_history += f"user: {message}\n"
-            prompt = message
-            self.open_ai.prompt_queue.put((prompt, self.context_history))  # 将聊天上下文作为第二个参数传递
+            self.context_history += f"user: {text}\n"
+            # 将聊天上下文作为第二个参数传递给openai
+            self.open_ai.prompt_queue.put((text, self.context_history))  
+            # 清楚输入框
             self.message_input.clear()
-
-            # 保存聊天记录到本地
-            self.save_chat_history()
 
     def keyPressEvent(self, event: QKeyEvent):
         if event.key() == Qt.Key_Return or event.key() == Qt.Key_Enter:
@@ -273,22 +361,20 @@ class ChatDialogBody(QDialog):
         else:
             super().keyPressEvent(event)
 
-    def handle_response(self, response, prompt):
+    def handle_response(self, response):
         if response and 'choices' in response:
-            pet_reply = self.open_ai.config["Pet"]["NICKNAME"] + ":" + response['choices'][0]['message']['content'].strip().replace('\n', '').replace('\r', '')
+            pet_reply = response['choices'][0]['message']['content'].strip().replace('\n', '').replace('\r', '')
             # 将 AI 回应添加到聊天上下文
             self.context_history += f"assistant: {pet_reply}\n"
         elif response and 'error' in response:
-            pet_reply = self.open_ai.config["Pet"]["NICKNAME"] + f": 发生错误 - {response['error']}"
+            pet_reply = f": 发生错误 - {response['error']}"
         else:
-            pet_reply = self.open_ai.config["Pet"]["NICKNAME"] + ":对不起，我无法回应您的问题，请稍后再试。"
-        self.append_message(message=pet_reply)
-        # 保存聊天记录到本地
-        self.save_chat_history()
+            pet_reply = ":对不起，我无法回应您的问题，请稍后再试。"
+        self.add_message("pet",pet_reply)
 
-    def save_chat_history(self):
+    def save_chat_history(self,message):
         with open(self.chat_log_file, "w", encoding="utf-8") as f:
-            f.write(self.chat_history.toPlainText())
+            f.write(message.text_label.text())
         print(f"聊天记录已保存到 {os.path.abspath(self.chat_log_file)}")
 
     
@@ -309,11 +395,6 @@ class ChatDialogBody(QDialog):
         self.context_history = ""
         # 创建一个新的聊天记录文件
         self.create_chat_log_file()
-
-    def closeEvent(self, event):
-        self.save_chat_history()
-        self.context_history = ""
-        event.accept()
 
     def closeEvent(self, event):
         self.save_chat_history()
