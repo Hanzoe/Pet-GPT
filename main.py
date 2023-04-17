e3643569c475c9a17202c7ae578b112e3b26880a
Hanzoe <2500066889@qq.com>
2023-04-17 11:14:25 +0800

    file

--- main.py
+++ main.py
@@ -354,14 +354,14 @@ class DesktopPet(QWidget):
         self.bubble.hide()
 
     def save_config(self):
-        with codecs.open(private_config, 'w', 'utf-8') as f:
+        with codecs.open(config_private, 'w', 'utf-8') as f:
             self.config.write(f) 
 
 if __name__ == '__main__':
     config_private = 'config_private.ini'
     app = QApplication(sys.argv)
     config = configparser.ConfigParser()
-    with codecs.open(private_config, 'r', 'utf-8') as f:
+    with codecs.open(config_private, 'r', 'utf-8') as f:
         # 读取配置文件内容
         config = configparser.ConfigParser()
         config.read_file(f)
